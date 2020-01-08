905e99dbaa105aaa2a007d020d48581e6060d748
Tyler Huang <thuang00@stuy.edu>
2020-01-07 22:11:05 -0500

    test

--- test.py
+++ test.py
@@ -1,4 +1,14 @@
+from flask import Flask, render_template
+import urllib, json
 from datetime import datetime
 now = datetime. now(). time()
 print("now =", now)
 print("type(now) =", type(now))
+
+def card():
+    u = urllib.request.urlopen('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+    response = u.read()
+    data = json.loads(response)
+    print(data['deck_id'])
+
+card()
