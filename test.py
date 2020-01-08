404135f86e176ec3d57666cab6d889a0e58a0345
Tyler Huang <thuang00@stuy.edu>
2020-01-08 09:08:25 -0500

    fixed error

--- test.py
+++ test.py
@@ -1,14 +1,24 @@
+headers = {
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+    'Accept-Encoding': 'none',
+    'Accept-Language': 'en-US,en;q=0.8',
+    'Connection': 'keep-alive'
+}
+
 from flask import Flask, render_template
-import urllib, json
+from urllib.request import urlopen, Request
+import json
 from datetime import datetime
 now = datetime. now(). time()
 print("now =", now)
 print("type(now) =", type(now))
 
 def card():
-    u = urllib.request.urlopen('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
-    response = u.read()
+    request = Request('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1',headers = headers)
+    response = urlopen(request).read()
     data = json.loads(response)
-    print(data['deck_id'])
+    return(data['deck_id'])
 
-card()
+print(card())
