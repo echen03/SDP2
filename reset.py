3a5b8dd82ab234da8c184fce8d780fee47c51903
Tyler Huang <thuang00@stuy.edu>
2020-01-07 21:32:13 -0500

    merge

--- reset.py
+++ reset.py
@@ -8,7 +8,7 @@ c = db.cursor()               #facilitate db ops
 command = "DROP TABLE IF EXISTS users;" # delete table
 c.execute(command)
 
-command = "CREATE TABLE IF NOT EXISTS users (id INTEGER, username TEXT, password TEXT, firstName TEXT, lastName TEXT, location TEXT, address TEXT);" # create table
+command = "CREATE TABLE IF NOT EXISTS users (id INTEGER, username TEXT, password TEXT, funds REAL, streak INTEGER);" # create table
 c.execute(command)    # run SQL statement
 
 db.commit()
